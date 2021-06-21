30d3d4759188e090b730f0a5d94a991f9094fd36
VipulKod <vipul.kodle@gmail.com>
2021-06-21 20:44:18 +0530

    Added proc file and some deployment details

--- restoapi/settings.py
+++ restoapi/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,3 +132,5 @@ CORS_ORIGIN_ALLOW_ALL = True
 # Media Folder Settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+django_heroku.settings(locals())
